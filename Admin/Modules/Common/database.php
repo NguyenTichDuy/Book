b3ec2abb98e3bd178b035360b3a60f01ee5e22d3
nguyenticduy <nguyentichduy10a15cmg@gmail.com>
2020-07-19 12:57:27 +0700

    +add some cange

--- Admin/Modules/Common/database.php
+++ Admin/Modules/Common/database.php
@@ -7,7 +7,8 @@ define('DB_NAME', 'reviewsach');
 try{
     $pdo = new PDO("mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME, DB_USERNAME, DB_PASSWORD);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e){
+} 
+catch(PDOException $e){
     die("ERROR: Could not connect. " . $e->getMessage());
 }
 ?>
